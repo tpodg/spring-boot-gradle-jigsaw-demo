c177c7a96a387a43827993bb2a20f55285f8656e
Tomaz Podgorsek <tomaz.podgorsek@bearingpoint.com>
2021-05-07 12:28:25 +0200

    Updated to spring boot 2.3.4.RELEASE

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,10 +1,10 @@
 object Config {
 
-    val boot_version = "2.1.1.RELEASE"
+    val boot_version = "2.3.4.RELEASE"
 
     object Plugins {
         val boot_version = Config.boot_version
-        val spring_dependency_management_version = "1.0.6.RELEASE"
+        val spring_dependency_management_version = "1.0.11.RELEASE"
         val modularity_version = "1.3.0"
     }
 }
